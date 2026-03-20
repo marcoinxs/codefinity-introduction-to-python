7cf0c843755df12c1954f1304ba02f4a4dedcb61
marcoinxs <>
2026-03-20 20:54:59 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,9 @@
+# Variables 
+Item =  "Bread" 
+item_price = 4.52
+items_in_stock = 230
+
+# Testing
+print("Item:", Item)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", items_in_stock)
